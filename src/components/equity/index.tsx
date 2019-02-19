cfb3501f451f0cfc51f9943200eecc3a6dd026db
LengYXin <lengyingxin8966@gmail.com>
2019-02-19 23:46:33 +0800

    update

--- src/components/equity/index.tsx
+++ src/components/equity/index.tsx
@@ -27,37 +27,37 @@ export default class extends Component<any, any> {
                     </View>
                 </View>
                 <View className="tr">
-                    <View className='at-row at-row--wrap'>
+                    <View className='at-row at-row--wrap tr-border'>
                         <View className='at-col at-col-3'>时间</View>
                         <View className='at-col at-col-3'>月</View>
                         <View className='at-col at-col-3'>年</View>
                         <View className='at-col at-col-3'>年</View>
                     </View>
-                    <View className='at-row at-row--wrap'>
+                    <View className='at-row at-row--wrap tr-border'>
                         <View className='at-col at-col-3'>限购</View>
                         <View className='at-col at-col-3'>1/份</View>
                         <View className='at-col at-col-3'>2/份</View>
                         <View className='at-col at-col-3'>无</View>
                     </View>
-                    <View className='at-row at-row--wrap'>
+                    <View className='at-row at-row--wrap tr-border'>
                         <View className='at-col at-col-3'>限量食材</View>
                         <View className='at-col at-col-3'></View>
                         <View className='at-col at-col-3'><Image src={Imgs.Duigou} className="img-g"/></View>
                         <View className='at-col at-col-3'><Image src={Imgs.Duigou} className="img-g"/></View>
                     </View>
-                    <View className='at-row at-row--wrap'>
+                    <View className='at-row at-row--wrap tr-border'>
                         <View className='at-col at-col-3'>专送</View>
                         <View className='at-col at-col-3'></View>
                         <View className='at-col at-col-3'><Image src={Imgs.Duigou} className="img-g"/></View>
                         <View className='at-col at-col-3'><Image src={Imgs.Duigou} className="img-g"/></View>
                     </View>
-                    <View className='at-row at-row--wrap'>
+                    <View className='at-row at-row--wrap tr-border'>
                         <View className='at-col at-col-3'>生日特权</View>
                         <View className='at-col at-col-3'></View>
                         <View className='at-col at-col-3'></View>
                         <View className='at-col at-col-3'><Image src={Imgs.Duigou} className="img-g"/></View>
                     </View>
-                    <View className='at-row at-row--wrap'>
+                    <View className='at-row at-row--wrap tr-border'>
                         <View className='at-col at-col-3'>管家服务</View>
                         <View className='at-col at-col-3'></View>
                         <View className='at-col at-col-3'></View>
