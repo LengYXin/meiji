22ee5a48990b21c49a67ad4e55f51fd305f8fd1a
liwan <liwan@samsundot.com>
2019-02-20 22:31:21 +0800

    我的

--- src/pages/user/core/index.tsx
+++ src/pages/user/core/index.tsx
@@ -1,8 +1,15 @@
-import { View } from '@tarojs/components';
+import { View, Image } from '@tarojs/components';
 import { observer } from '@tarojs/mobx';
 import Taro, { Component, Config } from '@tarojs/taro';
-import { AtList, AtListItem } from "taro-ui";
+import { AtList, AtListItem } from "taro-ui"
 import './index.less';
+import img from '../../../img/img47.png'
+import tabImg from '../../../img/img70.png'
+import upNoImg from '../../../img/img66.png'
+import upYesImg from '../../../img/img65.png'
+import SuoImg from '../../../img/img31.png'
+import code from '../../../img/code.png'
+import kf from '../../../img/kf.png'
 
 
 @observer
@@ -34,13 +41,59 @@ export default class extends Component {
 
   render() {
     return (
-      <View className='index'>
-        <AtList>
-          <AtListItem title='标题文字' />
-          <AtListItem title='标题文字' arrow='right' />
-          <AtListItem title='标题文字' extraText='详细信息' />
-          <AtListItem title='禁用状态' disabled extraText='详细信息' />
-        </AtList>
+      <View className='user-core'>
+        <View className="core-header">
+          <View className="header-left">
+            <Image className="left-img" src="" />
+          </View>
+          <View className="header-right">
+            <View className="right-name">白叶挽青湖</View>
+            <View className="right-img">
+              <Image src={img} />
+            </View>
+            <View className="right-time">2017.12.11到期</View>
+          </View>
+        </View>
+        <View className="core-tab">
+          <Image className="tab-img" src={tabImg} />
+          <View className="tab-list">
+            <View className="tab-txt">全部订单</View>
+            <View className="tab-txt">待付款</View>
+            <View className="tab-txt">待发货</View>
+            <View className="tab-txt">已发货</View>
+            <View className="tab-txt">已完成</View>
+          </View>
+        </View>
+        <View className="invitation">
+          <View className="invitation-title">
+            邀请码
+          </View>
+          <View className="invitation-updata">
+            <View className="updata-list">
+              <Image className="list-img" src={upYesImg} />
+            </View>
+            <View className="updata-list">
+              <Image className="list-img" src={upYesImg} />
+            </View>
+            <View className="updata-list">
+              <Image className="list-img" src={upNoImg} />
+              <Image className="list-img-s" src={SuoImg} />
+            </View>
+          </View>
+        </View>
+        <View className="core-list">
+          <AtList hasBorder={false}>
+            <AtListItem title='卡劵' arrow='right' hasBorder={false} />
+            <AtListItem title='收货地址' arrow='right' hasBorder={false} />
+            <AtListItem title='账户安全' arrow='right' hasBorder={false} />
+          </AtList>
+        </View>
+        <View className="core-code">
+          <Image className="code-img" src={code} />
+          <View className="code-txt">
+            <Image className="txt-img" src={kf} />联系客服
+        </View>
+        </View>
       </View>
     )
   }
