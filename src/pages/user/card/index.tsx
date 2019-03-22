633560c26f2bce355600489c6115aee5d3e79b65
LengYXin <lengyingxin8966@gmail.com>
2019-03-22 13:03:25 +0800

    错别字

--- src/pages/user/card/index.tsx
+++ src/pages/user/card/index.tsx
@@ -16,7 +16,7 @@ export default class extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config: Config = {
-    navigationBarTitleText: '卡卷'
+    navigationBarTitleText: '卡劵'
   }
 
   componentWillMount() { }
